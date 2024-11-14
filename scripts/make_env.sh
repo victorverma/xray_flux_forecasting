2382e0d4cbed65cdf5c95ff06cbdd704af906daa
victorverma <vishashv@gmail.com>
2024-11-14 16:35:32 -0500

    Don't create Jupyter kernel - don't need to

--- scripts/make_env.sh
+++ scripts/make_env.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-conda env create --prefix ../env/ -f ../env.yml
-conda activate ../env/
-python -m ipykernel install --user --name=env
-conda deactivate
+conda env create -p ../env/ -f ../env.yml
